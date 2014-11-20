ef73c0ff7d20613697472b2985f99d6e154576b0
Hylke Stapersma <hylke.stapersma@gmail.com>
2014-11-20 16:21:41 +0100

    First setup for exercise-08 puppet/docker on jenkins slave

---
+++ pipeline/job-004-puppet-code-quality.groovy
@@ -0,0 +1,12 @@
+job {
+    name 'Puppet code quality'
+    label 'puppet'
+    scm {
+        git('https://github.com/Hylke1982/TDDTrainingApplication', 'devops-experience-workshop')
+    }
+    steps {
+        shell('cd puppet')
+        shell('docker run puppet-test bundle install')
+        shell('docker run puppet-test bundle exec rake lint')
+    }
+}
