7fdc2ad10b064e11286a3495ce40a4fe1659551b
Hylke Stapersma <hylke.stapersma@gmail.com>
2014-12-03 20:07:15 +0100

    Job puppet pipeline configuration

--- pipeline/job-004-puppet-code-quality.groovy
+++ pipeline/job-004-puppet-code-quality.groovy
@@ -3,7 +3,7 @@ job {
     customWorkspace '${JENKINS_HOME}/workspace/puppet-code-quality'
     label 'docker'
     steps {
-        copyArtifacts 'First build job (compilation)', 'puppet/**', '', false, true
+        copyArtifacts downstreamUnityJob.name, 'puppet/**'
         shell('docker run --rm -v /home/jenkins-slave/workspace/puppet-code-quality/puppet:/puppet -t puppet-test')
     }
 }
