88a0a0afaf3bd0080b7a5e9b6ba0ae30a5d85dfa
Hylke Stapersma <hylke.stapersma@gmail.com>
2014-12-08 15:51:17 +0100

    Added integration tests

---
+++ pipeline/job-006-integration-test.groovy
@@ -0,0 +1,15 @@
+job {
+    name 'Integration tests'
+    blockOn('Second build job (unit test)\nJavaScript unit test')
+    blockOnUpstreamProjects()
+    scm {
+        cloneWorkspace('First build job (compilation)')
+    }
+    steps {
+        maven('-f TDDTrainingApplicationCC/pom.xml test -Pintegration')
+    }
+    publishers {
+//        downstream('Checkstyle validation')
+        archiveJunit('**/target/surefire-reports/*.xml')
+    }
+}
