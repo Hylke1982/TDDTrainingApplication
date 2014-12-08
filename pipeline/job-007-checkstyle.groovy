cccc6a541f7b9426d17735fb10e400c642209a52
Hylke Stapersma <hylke.stapersma@gmail.com>
2014-12-08 15:56:09 +0100

    Added QA

---
+++ pipeline/job-007-checkstyle.groovy
@@ -0,0 +1,15 @@
+job {
+    name 'Checkstyle validation'
+    scm {
+        cloneWorkspace('First build job (compilation)')
+    }
+    steps {
+        maven('-pl TDDTrainingApplicationCC checkstyle:checkstyle')
+    }
+    publishers {
+//        downstream('Artifact creation')
+        checkstyle('**/checkstyle-result.xml') {
+            shouldDetectModules true
+        }
+    }
+}
